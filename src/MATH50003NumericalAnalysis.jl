dc73764b367c065b0baf1559621ab4f697dabf91
Sheehan Olver <solver@mac.com>
2021-11-08 21:49:56 +0000

    update

--- src/MATH50003NumericalAnalysis.jl
+++ src/MATH50003NumericalAnalysis.jl
@@ -1,3 +1,5 @@
 using Weave
 
-notebook("src/1FloatingPoint.jmd"; out_path="notebooks/", jupyter_path="/Users/solver/.julia/conda/3/bin/jupyter")
+notebook("src/1Overview.jmd"; out_path="notebooks/", jupyter_path="/Users/solver/.julia/conda/3/bin/jupyter")
+notebook("src/2Julia.jmd"; out_path="notebooks/", jupyter_path="/Users/solver/.julia/conda/3/bin/jupyter")
+notebook("src/3FloatingPoint.jmd"; out_path="notebooks/", jupyter_path="/Users/solver/.julia/conda/3/bin/jupyter")
