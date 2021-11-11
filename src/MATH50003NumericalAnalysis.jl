12aa7383bec7d671f30801d6fa58b6c1afe25f27
Sheehan Olver <solver@mac.com>
2021-11-11 11:57:05 +0000

    Work on Julia description

--- src/MATH50003NumericalAnalysis.jl
+++ src/MATH50003NumericalAnalysis.jl
@@ -1,6 +1,7 @@
 using Weave
 
-kwds = (out_path="notebooks/", jupyter_path="/Users/solver/.julia/conda/3/bin/jupyter", nbconvert_options="--allow-errors")
+kwds = (out_path="notebooks/", jupyter_path="$(homedir())/.julia/conda/3/bin/jupyter", nbconvert_options="--allow-errors")
+
 notebook("src/1Overview.jmd"; kwds...)
 notebook("src/2Julia.jmd"; kwds...)
 notebook("src/3FloatingPoint.jmd"; kwds...)
