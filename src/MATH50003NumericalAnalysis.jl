193c8de41902e7adcc239fbdacdedfb4e3b0e05d
Sheehan Olver <solver@mac.com>
2021-11-19 15:43:36 +0000

    reorg

--- src/MATH50003NumericalAnalysis.jl
+++ src/MATH50003NumericalAnalysis.jl
@@ -2,6 +2,5 @@ using Weave
 
 kwds = (out_path="notebooks/", jupyter_path="$(homedir())/.julia/conda/3/bin/jupyter", nbconvert_options="--allow-errors")
 
-notebook("src/1Overview.jmd"; kwds...)
-notebook("src/2Julia.jmd"; kwds...)
-notebook("src/3FloatingPoint.jmd"; kwds...)
+notebook("src/Julia.jmd"; kwds...)
+notebook("src/FloatingPoint.jmd"; kwds...)
