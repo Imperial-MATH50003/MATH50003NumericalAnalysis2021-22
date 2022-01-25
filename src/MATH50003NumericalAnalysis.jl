16a679e41aa6dae905a9050bc818eedf3cbae7a5
Sheehan Olver <solver@mac.com>
2022-01-25 22:54:00 +0000

    Work on Cholesky

--- src/MATH50003NumericalAnalysis.jl
+++ src/MATH50003NumericalAnalysis.jl
@@ -1,42 +1,43 @@
 using Weave
 
+nkwds = (out_path = "notebooks/", jupyter_path = "$(homedir())/.julia/conda/3/bin/jupyter", nbconvert_options = "--allow-errors")
+skwds = (out_path = "sheets/", jupyter_path = "$(homedir())/.julia/conda/3/bin/jupyter", nbconvert_options = "--allow-errors")
 
 ##
 # notes
 ##
 
-kwds = (out_path = "notebooks/", jupyter_path = "$(homedir())/.julia/conda/3/bin/jupyter", nbconvert_options = "--allow-errors")
 
-notebook("src/Julia.jmd"; kwds...)
-notebook("src/Asymptotics.jmd"; kwds...)
 
-notebook("src/Numbers.jmd"; kwds...)
-notebook("src/Differentiation.jmd"; kwds...)
+notebook("src/Julia.jmd"; nkwds...)
+notebook("src/Asymptotics.jmd"; nkwds...)
 
-notebook("src/StructuredMatrices.jmd"; kwds...)
-notebook("src/Decompositions.jmd"; kwds...)
-notebook("src/Norms.jmd"; kwds...)
-notebook("src/DifferentialEquations.jmd"; kwds...)
+notebook("src/Numbers.jmd"; nkwds...)
+notebook("src/Differentiation.jmd"; nkwds...)
 
-notebook("src/Fourier.jmd"; kwds...)
-notebook("src/OrthogonalPolynomials.jmd"; kwds...)
-notebook("src/Interpolation.jmd"; kwds...)
-notebook("src/Integration.jmd"; kwds...)
+notebook("src/StructuredMatrices.jmd"; nkwds...)
+notebook("src/Decompositions.jmd"; nkwds...)
+notebook("src/Norms.jmd"; nkwds...)
+notebook("src/DifferentialEquations.jmd"; nkwds...)
+
+notebook("src/Fourier.jmd"; nkwds...)
+notebook("src/OrthogonalPolynomials.jmd"; nkwds...)
+notebook("src/Interpolation.jmd"; nkwds...)
+notebook("src/Integration.jmd"; nkwds...)
 
 
 ##
 # problem sheets
 ##
 
-kwds = (out_path = "sheets/", jupyter_path = "$(homedir())/.julia/conda/3/bin/jupyter", nbconvert_options = "--allow-errors")
-notebook("src/week1.jmd"; kwds...)
-notebook("src/week2.jmd"; kwds...)
-notebook("src/advanced1.jmd"; kwds...)
+notebook("src/week1.jmd"; skwds...)
+notebook("src/week2.jmd"; skwds...)
+notebook("src/advanced1.jmd"; skwds...)
 
 ##
 # solutions 
 ###
 
-notebook("src/week1s.jmd"; kwds...)
-notebook("src/week2s.jmd"; kwds...)
-notebook("src/week3s.jmd"; kwds...)
+notebook("src/week1s.jmd"; skwds...)
+notebook("src/week2s.jmd"; skwds...)
+notebook("src/week3s.jmd"; skwds...)
